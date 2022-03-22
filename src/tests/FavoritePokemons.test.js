1d263cdf1c638bf6a57e933d76dcf09f7ee6e90f
Felipe Gama Danta <epilefgama2@gmail.com>
2022-03-22 17:15:49 -0300

    teste para o componente FavoritePokemons, requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,27 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import FavoritePokemons from '../components/FavoritePokemons';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+describe('', () => {
+  it('', () => {
+    renderWithRouter(<FavoritePokemons />);
+    const text = screen.getByText(/no favorite pokemon found/i);
+    expect(text).toBeInTheDocument();
+  });
+
+  it('', () => {
+    const { history } = renderWithRouter(<App />);
+    const detailsLink = screen.getByRole('link', { name: /more details/i });
+    userEvent.click(detailsLink);
+
+    const favoriteButton = screen.getByText(/pokémon favoritado\?/i);
+    userEvent.click(favoriteButton);
+
+    history.push('/favorites');
+    const allImages = screen.getAllByRole('link', { name: /more details/i });
+    expect(allImages).toHaveLength(1);
+  });
+});
